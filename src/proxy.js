a8ed1830f98f40371973c4bdf3ce75679d2c95af
Towfiqul Islam <towfiqulislam017399@gmail.com>
2026-05-02 10:34:55 +0600

    better-auth error fix

--- src/proxy.js
+++ src/proxy.js
@@ -1,5 +1,5 @@
-import dns from "node:dns";
-dns.setServers(['8.8.8.8', '8.8.4.4']);
+// import dns from "node:dns";
+// dns.setServers(['8.8.8.8', '8.8.4.4']);
 import { NextResponse } from "next/server";
 import { headers } from "next/headers";
 import { auth } from "./lib/auth";
